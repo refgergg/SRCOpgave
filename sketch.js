c028085c18b5c1f68917bdd53bd5d377ac0f2062
refgergg <kaspersbruger123@gmail.com>
2025-02-06 10:49:01 +0100

    Fixed snapping and added button stuff

--- sketch.js
+++ sketch.js
@@ -21,6 +21,8 @@ let pd=20
 let bezierPoints = [];
 let curves = [];
 
+let snapDistance = 35;
+
 function setup() {
   createCanvas(400, 400);
   //console.log(crlPtReduceDeCasteljau(points, 0));
@@ -47,13 +49,30 @@ function draw() {
   let button = createButton('Finish Curve');
   button.position(500, 300);
   button.mousePressed(finishCurve);
+
+  let button2 = createButton('Reset Canvas');
+  button2.position(500, 200);
+  button2.mousePressed(resetCanvas);
+
+  let button3 = createButton('Undo Change');
+  button3.position(500, 250);
+  button3.mousePressed(undoChange);
   text("Click & drag the points to change the bézier curve",50,375)
+}
+
+function undoChange(){
+  curves.splice(-1,1);
 }
 
 function finishCurve(){
   curves.push(new Curve(bezierPoints, bezierPoints[0], bezierPoints[bezierPoints.length-1]));
   bezierPoints = [];
   console.log(curves);
+}
+
+function resetCanvas(){
+  curves = [];
+  bezierPoints = [];
 }
 
 function createPoints(x, y){
@@ -75,63 +94,48 @@ function drawPoints(){
   }
 }
 
-function movePoint(){
-  for(let i=0; i<bezierPoints.length;i++){
-    if(bezierPoints[i].relativX!=undefined){
-      bezierPoints[i].x=mouseX+bezierPoints[i].relativX
-      bezierPoints[i].y=mouseY+bezierPoints[i].relativY
-    }
-  } 
-}
-
 function mousePressed(){
-  /*
-  for(let i=0; i<bezierPoints.length;i++){
-    if(dist(bezierPoints[i].x,bezierPoints[i].y,mouseX,mouseY)<pd/2){
-      bezierPoints[i].relativX=bezierPoints[i].x-mouseX
-      bezierPoints[i].relativY=bezierPoints[i].y-mouseY
-    }
-  } 
-  */
   if(mouseX < width && mouseY < height) {
-    
     closestPointAndDist = [0,0];
     //check if our placement of point is close to another point. If so, snap to it
     //this code is disgusting. Unfortunately i dont care
     for(let i=0;i<curves.length;i++){
-      for(let j=0;j<curves[i].points.length;j++){
-        //Find current curve point dist
-        pointDist = dist(mouseX, mouseY, curves[i].points[j].x, curves[i].points[j].y);
-        if(closestPointAndDist[1] == 0) {
-          closestPointAndDist = [curves[i].points[j], pointDist];
-          continue;
+      //Find current curve point dist
+      pointDistStartPoint = dist(mouseX, mouseY, curves[i].startPoint.x, curves[i].startPoint.y);
+      /*console.log("dist to start point");
+      console.log(pointDistStartPoint);
+      console.log("mouse x and y: " + [mouseX,mouseY]);
+      console.log("start point x and y: " + [curves[i].startPoint.x, curves[i].startPoint.y]);*/
+      pointDistEndPoint = dist(mouseX, mouseY, curves[i].endPoint.x, curves[i].endPoint.y);
+      if(closestPointAndDist[1] == 0) {
+        if(pointDistStartPoint < pointDistEndPoint) {
+          closestPointAndDist = [curves[i].startPoint, pointDistStartPoint];
         } else {
-          //If current curve point dist is closer than closest so far, replace
-          if(closestPointAndDist[1] > pointDist) {
-            closestPointAndDist = [curves[i].points[j], pointDist];
-          }
+          closestPointAndDist = [curves[i].endPoint, pointDistEndPoint];
         }
-      }
+      } 
     }
 
-    if(closestPointAndDist[1] < 25 && closestPointAndDist[1] > 0) {
+    console.log(closestPointAndDist);
+    if(closestPointAndDist[1] < snapDistance && closestPointAndDist[1] > 0) {
       createPoints(closestPointAndDist[0].x, closestPointAndDist[0].y);
+      console.log("Close, snapping!");
     } else {
       createPoints(mouseX, mouseY)
     }
   }
 }
 
-function mouseReleased(){
+/*function mouseReleased(){
   for(let i=0; i<bezierPoints.length;i++){
     bezierPoints[i].relativX=undefined
     bezierPoints[i].relativY=undefined
   } 
-}
+}*/
 
 
 /////////// version til et virkårligt antal punkter
-let points = [ [0, 128], [128, 0], [256, 0], [384, 128] ]
+//let points = [ [0, 128], [128, 0], [256, 0], [384, 128] ]
 function drawDecasteljau(points){
   if(points <= 1){return;}
   for(let i = 0; i < 1; i+=0.001){
